1037b35605e62ce5156ee1d1e530cf01216b27b4
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-08-06 10:48:23 +0530

    Question 1

--- question1/main.cpp
+++ question1/main.cpp
@@ -2,3 +2,30 @@
 and to tell him what grade the user recieved */ 
 
 #include <iostream>
+#include <typeinfo>
+using namespace std ;
+int main(){
+    int marks;
+    float sum=0;
+    for(int i=1;i<6;i++){
+        cout << "Enter the marks of the 5 Subject :";
+        cin >> marks;
+        sum=sum+marks;
+    }
+    float grade=(sum/500)*100;
+
+    if(grade >=90.00){
+          cout << "Your Grade is A+";
+    }else if (grade >=80.00 && grade < 90.00){
+        cout << "Your Grade is A";
+    }else if (grade >=70.00 && grade <80.00){
+        cout << "Your Grade is B";
+    }else if (grade >=60.00 && grade <70.00){
+        cout << "Your Grade is C";
+    }else if(grade >=50.00 && grade <60.00){
+        cout <<"Your Grade is D";
+    }
+    else{
+        cout<<"Your grade is E you are failed ";
+    }
+}
